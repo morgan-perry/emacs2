662b231d58c5ce4ccef128aba7931f97d18fa50a
Morgan Perry <morganperry@posteo.net>
2025-06-22 19:58:11 +1000

    Jinx spell checker

--- init.el
+++ init.el
@@ -173,6 +173,15 @@
 
 (use-package magit
   :straight t)
+(use-package jinx
+  :straight t
+  :hook (emacs-startup . global-jinx-mode)
+  :general
+  ("M-$" #'jinx-correct)
+  (:state 'normal
+	  "C-;" #'jinx-correct)
+  :config
+  (setq jinx-languages "en-custom"))
 
 (use-package git-auto-commit-mode
   :straight t)
